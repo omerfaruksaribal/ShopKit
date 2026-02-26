7d9e919d21950264382b7e66a6b2d1af1a8d1996
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2026-02-26 15:22:40 +0300

    implementation ios

---
+++ shopkit-ios/shopkit-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shopkit-ios
+//
+//  Created by Ömerfaruk Saribal on 26.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
